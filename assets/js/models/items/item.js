dc97007aae64e56740a59fa83ebf5683f79e462d
CrisArranz <c.arranz.arevalo@gmail.com>
2024-10-28 13:53:00 +0100

    [PACMAN] ADd collisions with items

--- assets/js/models/items/item.js
+++ assets/js/models/items/item.js
@@ -9,6 +9,18 @@ class Item extends NonAnimatedSprites {
       color,
       SHAPES.circle,
       radius
-    )
+    );
+
+    this.eaten = false;
+  }
+
+  collision(character) {
+    const collisionX = (character.positionX + 20) === this.positionX && (character.positionX + character.width - 20) === this.positionX;
+    const collisionY = (character.positionY + 20) === this.positionY && (character.positionY + character.height - 20) === this.positionY;
+    return collisionX && collisionY;
+  }
+
+  hasEaten() {
+    this.eaten = true;
   }
 }
